7b571bd9bf7063d4b19b2e9ee7f9ea1d797f85f7
TanguyHue <106544754+TanguyHue@users.noreply.github.com>
2024-03-12 16:58:49 +0100

    video arriere plan home

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
       spacing: {
         '180px': '180px',
         '148px': '148px',
+        '52px': '52px',
+        '68px': '68px',
+        'screenH': 'calc(100vh - 68px)',
       },
     },
   },
