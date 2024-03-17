7457d4df3f3b948d580791d06de1aee2a30038db
TanguyHue <106544754+TanguyHue@users.noreply.github.com>
2024-03-17 17:55:10 +0100

    affichage calendrier dynamique

--- app/controllers/admin/admin_controller.ts
+++ app/controllers/admin/admin_controller.ts
@@ -1,4 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import { DateTime } from 'luxon'
 
 export default class AdminController {
   index({ view }: HttpContext) {
@@ -6,7 +7,35 @@ export default class AdminController {
   }
 
   agenda({ view }: HttpContext) {
-    return view.render('admin/agenda/index')
+    const date = new Date()
+    const day = date.getDate()
+    const month = date.getMonth()
+    const year = date.getFullYear()
+
+    const firstDayOfMonth = new Date(year, month, 1).getDay()
+    const numberDaysInMonth = DateTime.fromObject({ year, month: month + 1 }).daysInMonth
+
+    if (!numberDaysInMonth) {
+      throw new Error('Invalid month')
+    }
+
+    console.log(firstDayOfMonth, numberDaysInMonth)
+
+    let days = []
+    for (let i = 1; i < 7 * 5 + 1; i++) {
+      if (i < firstDayOfMonth) {
+        days.push('')
+      } else if (i > numberDaysInMonth + firstDayOfMonth - 1) {
+        days.push('')
+      } else {
+        days.push(i - firstDayOfMonth + 1)
+      }
+    }
+
+    console.log(days)
+
+    const today = `${year}-${month + 1}-${day}`
+    return view.render('admin/agenda/index', { today, days })
   }
 
   taches({ view }: HttpContext) {
